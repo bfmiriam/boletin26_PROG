346f417220a5ffdaa3b53aa2ca02a9f701ff1c74
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2018-04-18 09:37:39 +0200

    boletin 26

---
+++ src/boletin26/Boletin26.java
@@ -0,0 +1,16 @@
+
+package boletin26;
+
+
+public class Boletin26 {
+
+   
+    public static void main(String[] args) {
+        
+        Formulario obx = new Formulario();
+        
+        obx.setVisible(true);
+        
+    }
+    
+}
